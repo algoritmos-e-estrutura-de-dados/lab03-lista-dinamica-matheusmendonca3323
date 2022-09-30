612a659d867e5d809288f62e9ef810e2d3e96b43
matheusmendonca3323 <110430944+matheusmendonca3323@users.noreply.github.com>
2022-09-30 14:48:28 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ lista = Lista()
 
 lista.adicionar(1)
 lista.adicionar(2)
-lista.adicionar(3)
+lista.adicionar(9)
+lista.adicionar(9)
+lista.adicionar(4)
+lista.adicionar(9)
+
+lista.remover_todos(9)
 
 lista.show()
